e3043ceb79783763e1a26361e5ea37d78e4c798b
Dino <dino15031992@gmail.com>
2022-09-28 13:59:26 +0200

    Initial commit

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name = " Dino ";
+        String surname = " Petrucci ";
+        System.out.println(name);
+        System.out.println(surname);
+
+    }
+}
